ca0bff166f0f785e43a35095691dadaeabf16567
Lucas <lucas.de@gmail.com>
2020-08-30 18:23:39 +0200

    Initial commit

---
+++ src/main/java/com/example/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityApplication.class, args);
+    }
+
+}
